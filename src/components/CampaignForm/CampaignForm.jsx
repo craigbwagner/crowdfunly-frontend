8b8bc5a61229d2287ca9ef8dda57e069cb19d73b
jcao91 <jcao91@yahoo.com>
2024-07-11 09:07:34 -0500

    add CampaignForm and link

---
+++ src/components/CampaignForm/CampaignForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const CampaignForm = () => {
+    const [formData, setFormData] = useState({
+
+    })
+
+
+
+    return(
+        <>
+        <main>
+
+        <h1>Campaign Form</h1>
+        </main>
+        
+        
+        
+        </>
+    )
+}
+
+export default CampaignForm;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const NavBar = ({ user, handleSignout }) => {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+            <li><Link to="/campaigns/new">Create Campaign</Link></li>
           </ul>
         </nav>
       ) : (
