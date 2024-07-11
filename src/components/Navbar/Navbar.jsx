c401bc98011f5abbc344ddc2a0220cfc4668a667
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-11 14:15:03 -0400

    add navbar links to browse campaigns, create campaign, and profile pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const NavBar = ({ user, handleSignout }) => {
+  const { userId } = useParams();
   return (
     <>
-      { user ? (
+      {user ? (
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/campaigns">Browse Campaigns</Link>
+            </li>
+            <li>
+              <Link to="/campaigns/create-campaign">Create Campaign</Link>
+            </li>
+            <li>
+              <Link to={`/profile/${user._id}`}>Profile</Link>
+            </li>
+            <li>
+              <Link to="" onClick={handleSignout}>
+                Sign Out
+              </Link>
+            </li>
           </ul>
         </nav>
       ) : (
         <nav>
           <ul>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
           </ul>
         </nav>
       )}
     </>
-  )
-}
+  );
+};
 
 export default NavBar;
