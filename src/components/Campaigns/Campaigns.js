00b3b4218825e8b78b7534ae50daf5a435359033
matthew-estes <matthew.estes19@gmail.com>
2024-07-11 14:47:10 -0400

    initialize state for campaigns and loading status

--- src/components/Campaigns/Campaigns.js
+++ src/components/Campaigns/Campaigns.js
@@ -1,1 +1,19 @@
 import { useState, useEffect } from "react"
+
+
+
+const Campaigns = () => {
+    const [campaigns, setCampaigns] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+
+    return (
+
+
+
+
+        
+    )
+
+}
