6ac67785e2357535e43f47072dcfbc6ec28717cf
matthew-estes <matthew.estes19@gmail.com>
2024-07-11 15:20:53 -0400

    map through campaigns and render each one and export the component

---
+++ src/components/CampaignsList/CampaignsList.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function CampaignList(props) {
+  return (
+    <div>
+      <h1>Available Campaigns</h1>
+      <ul>
+        {props.campaigns.map((campaign) => (
+          <li key={campaign._id}>
+            <h2>{campaign.title}</h2>
+            <p>{campaign.description}</p>
+            <p>Goal: ${campaign.goalAmount}</p>
+            <p>Raised: ${campaign.amountRaised}</p>
+            <p>End Date: {new Date(campaign.endDate).toLocaleDateString()}</p>
+            <p>Type: {campaign.campaignType}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CampaignList;
