493a436f6ed2ebd567b739eeba533ed40179bc31
matthew-estes <matthew.estes19@gmail.com>
2024-07-12 09:56:07 -0400

    add index function to fetch all campaigns

--- src/services/campaignService.js
+++ src/services/campaignService.js
@@ -1,1 +1,13 @@
 const BASE_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/campaigns`;
+
+const index = async () => {
+    try {
+      const res = await fetch(BASE_URL, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
