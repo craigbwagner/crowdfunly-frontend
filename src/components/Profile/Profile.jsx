389e9b7b5b35915e72bf6bce5b9084a13c7b1619
matthew-estes <matthew.estes19@gmail.com>
2024-07-12 15:32:57 -0400

    create jsx structure to render any campaigns if created by the user

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,31 @@
+import { Link } from "react-router-dom";
 
+const Profile = ({ user, campaigns }) => {
+  const userCampaigns = campaigns.filter((campaign) => campaign.createdBy._id === user._id);
 
-
-const Profile = ({ user }) => {
   return (
-    <>
+    <div>
       <h1>{user.username}'s Profile</h1>
-  
-      <h2>Campaigns</h2>
-      <h2>Contributions</h2>
-    </>
+
+      <h2>Created Campaigns</h2>
+      {userCampaigns.length > 0 ? (
+        <ul>
+          {userCampaigns.map((campaign) => (
+            <li key={campaign._id}>
+              <Link to={`/campaigns/${campaign._id}`}>{campaign.title}</Link>
+              <p>{campaign.description}</p>
+              <p>Goal: ${campaign.goalAmount}</p>
+              <p>Raised: ${campaign.amountRaised}</p>
+              <p>End Date: {new Date(campaign.endDate).toLocaleDateString()}</p>
+              <p>Type: {campaign.campaignType}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No campaigns created yet.</p>
+      )}
+    </div>
   );
-}
+};
 
 export default Profile;
