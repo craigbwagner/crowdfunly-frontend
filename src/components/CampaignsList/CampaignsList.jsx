ea0503f6d42c13f949318d71b8d5adb54234e625
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-12 10:58:31 -0400

    enable display of campaigns on CampaignsList

--- src/components/CampaignsList/CampaignsList.jsx
+++ src/components/CampaignsList/CampaignsList.jsx
@@ -3,7 +3,7 @@ function CampaignsList(props) {
     <div>
       <h1>Available Campaigns</h1>
       <ul>
-        {/* {props.campaigns.map((campaign) => (
+        {props.campaigns.map((campaign) => (
           <li key={campaign._id}>
             <h2>{campaign.title}</h2>
             <p>{campaign.description}</p>
@@ -12,7 +12,7 @@ function CampaignsList(props) {
             <p>End Date: {new Date(campaign.endDate).toLocaleDateString()}</p>
             <p>Type: {campaign.campaignType}</p>
           </li>
-        ))} */}
+        ))}
       </ul>
     </div>
   );
