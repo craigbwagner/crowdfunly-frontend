6dbc7ad83f3fbb6fae50e7d37bbb8cabbfb5a768
matthew-estes <matthew.estes19@gmail.com>
2024-07-12 14:19:41 -0400

    remove joined

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = ({ user }) => {
   return (
     <>
       <h1>{user.username}'s Profile</h1>
-      <h3>Joined:</h3>
+  
       <h2>Campaigns</h2>
       <h2>Contributions</h2>
     </>
