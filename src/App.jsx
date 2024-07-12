a549699d90e5a93d345166b3f72f752888251557
matthew-estes <matthew.estes19@gmail.com>
2024-07-12 10:15:25 -0400

    define handleDeleteCampaign function to delete a campaign and update state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Dashboard from './components/Dashboard/Dashboard';
-import Landing from './components/Landing/Landing';
-import SignupForm from './components/SignupForm/SignupForm';
-import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService';
-import CampaignList from './components/CampaignList';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+import * as authService from "../src/services/authService";
+import CampaignList from "./components/CampaignList";
 import CampaignForm from "./components/CampaignForm/CampaignForm";
 import Profile from "./components/Profile/Profile";
-import * as campaignService from '../src/services/campaignService';
-
-
+import * as campaignService from "../src/services/campaignService";
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
@@ -21,6 +19,12 @@ const App = () => {
   const handleAddCampaign = async (campaignFormData) => {
     const newCampaign = await create(campaignFormData);
     setCampaigns([newCampaign, ...campaigns]);
+    navigate("/campaigns");
+  };
+
+  const handleDeleteCampaign = async (campaignId) => {
+    const deleteCampaign = await deleteCampaign(campaignId);
+    SetCampaigns(campaigns.filter((campaign) => campaign._id !== deletedCampaign._id));
     navigate("/campaigns");
   };
 
@@ -43,13 +47,9 @@ const App = () => {
         )}
 
         <Route path="/campaigns" element={<CampaignList campaigns={campaigns} />} />
-        <Route
-          path="/campaigns/create-campaign"
-          element={<CampaignForm handleAddCampaign={handleAddCampaign} />}
-        />
-        <Route path='/signup' element={<SignupForm setUser={setUser} />} />
-        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
-        
+        <Route path="/campaigns/create-campaign" element={<CampaignForm handleAddCampaign={handleAddCampaign} />} />
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
       </Routes>
     </>
   );
