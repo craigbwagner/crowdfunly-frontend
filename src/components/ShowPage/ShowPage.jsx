c9faaed0ffadd3538943e42a95b37ffd05b70f81
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-12 11:38:51 -0400

    add essential details to campaign show page

--- src/components/ShowPage/ShowPage.jsx
+++ src/components/ShowPage/ShowPage.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import * as campaignService from "../../services/campaignService";
+import { useEffect, useState } from "react";
 
-function ShowPage({ campaign }) {
+function ShowPage() {
+  const [campaign, setCampaign] = useState(null);
+  const { campaignId } = useParams();
+
+  useEffect(() => {
+    const fetchCampaign = async () => {
+      const campaignData = await campaignService.show(campaignId);
+      setCampaign(campaignData);
+    };
+    fetchCampaign();
+  }, [campaignId]);
+  console.log(campaign);
+
+  if (!campaign) return <main>Loading...</main>;
   return (
     <>
-      <div>{campaign.title}</div>
+      <h1>{campaign.title}</h1>
+      <h2>{campaign.campaignType}</h2>
+      <h3>Goal: {campaign.goalAmount}</h3>
+      <h3>Amount Raised: {campaign.amountRaised}</h3>
+      <h3>Created by: {campaign.createdBy.username}</h3>
+      <h3>
+        Created:{" "}
+        {new Date(campaign.createdAt).toLocaleDateString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          timeZone: "UTC",
+        })}
+      </h3>
+      <h3>
+        End Date:{" "}
+        {new Date(campaign.endDate).toLocaleDateString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          timeZone: "UTC",
+        })}
+      </h3>
+      <p>{campaign.description}</p>
+      <h2>Contributions</h2>
     </>
   );
 }
