4b9b45ab0d96233545aa1ac4635dedc5e414d434
matthew-estes <matthew.estes19@gmail.com>
2024-07-12 11:27:27 -0400

    refactored to simplify nav options

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 
 const NavBar = ({ user, handleSignout }) => {
-
   return (
-    <>
-      {user ? (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/campaigns">Browse Campaigns</Link>
-            </li>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/campaigns">Browse Campaigns</Link>
+        </li>
+        {user ? (
+          <>
             <li>
               <Link to="/campaigns/new">Create Campaign</Link>
             </li>
@@ -20,31 +19,21 @@ const NavBar = ({ user, handleSignout }) => {
               <Link to={`/profile/${user._id}`}>Profile</Link>
             </li>
             <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link>
+              <Link to="" onClick={handleSignout}>Sign Out</Link>
             </li>
-          </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
-          <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/campaigns">Browse Campaigns</Link>
-            </li>
+          </>
+        ) : (
+          <>
             <li>
               <Link to="/signin">Sign In</Link>
             </li>
             <li>
               <Link to="/signup">Sign Up</Link>
             </li>
-          </ul>
-        </nav>
-      )}
-    </>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 };
 
