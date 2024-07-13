6355819ef2df5c692aa2a95c7c40b67dedc29273
jcao91 <jcao91@yahoo.com>
2024-07-13 15:51:03 -0500

    create formatDate function to properly show on edit page of campaign form

--- src/components/CampaignForm/CampaignForm.jsx
+++ src/components/CampaignForm/CampaignForm.jsx
@@ -38,6 +38,14 @@ const CampaignForm = (props) => {
     if (campaignId) fetchCampaign();
   }, [campaignId]);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
   return (
     <>
       <main>
