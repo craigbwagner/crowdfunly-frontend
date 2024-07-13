ec51be37ae323bdb21512cf87ec03b469cbc42b3
jcao91 <jcao91@yahoo.com>
2024-07-13 15:54:05 -0500

    resolved edit campaign form date issue

--- src/components/CampaignForm/CampaignForm.jsx
+++ src/components/CampaignForm/CampaignForm.jsx
@@ -41,8 +41,8 @@ const CampaignForm = (props) => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   };
 
