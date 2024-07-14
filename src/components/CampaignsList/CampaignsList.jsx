42584d9c3fd8dedf192b7dffccb6706d211d4ffb
matthew-estes <matthew.estes19@gmail.com>
2024-07-14 11:31:46 -0400

    resolve load error by using import.meta instead of process

--- src/components/CampaignsList/CampaignsList.jsx
+++ src/components/CampaignsList/CampaignsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './CampaignsList.css';
+
 
 function CampaignsList({ campaigns }) {
   return (
--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -1,51 +1,63 @@
-import React, { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import React, { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 const PaymentForm = () => {
-    const [success, setSuccess] = useState(false);
-    const stripe = useStripe();
-    const elements = useElements();
+  const [success, setSuccess] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: 'card',
-            card: elements.getElement(CardElement),
-          });
-        
-        if (!error) {
-            try {
-                const { id } = paymentMethod;
-                const clientSecret = await simulateCreatePaymentIntent();
-                const confirmPayment = await stripe.confirmCardPayment(clientSecret, {
-                    payment_method: id, 
-                });
-                if (confirmPayment.error) {
-                    console.log(confirmPayment.error.message);
-                } else {
-                    setSuccess(true);
-                }
-            } catch (error) {
-                console.log(error)
-            } else {
-                console.log(error.message)
-            }
-        };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        const simulateCreatePaymentIntent = async () => {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve('simulated_client_secret');
-                }, 1000);
-                });
-            };
-        };
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
 
-        return (
+    if (!error) {
+      try {
+        const { id } = paymentMethod;
+        const clientSecret = await simulateCreatePaymentIntent();
+        const confirmPayment = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: id,
+        });
+        if (confirmPayment.error) {
+          console.log(confirmPayment.error.message);
+        } else {
+          setSuccess(true);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
 
+    const simulateCreatePaymentIntent = async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("simulated_client_secret");
+        }, 1000);
+      });
+    };
+  };
 
-        );
-
-
-}
+  return (
+    <div>
+      {!success ? (
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div>
+              <CardElement />
+            </div>
+          </fieldset>
+          <button type="submit" disabled={!stripe}>
+            Pay
+          </button>
+        </form>
+      ) : (
+        <div>
+          <h2>Payment Successful!</h2>
+        </div>
+      )}
+    </div>
+  );
+};
