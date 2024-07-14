4a2450bf942804dcaa994baa29d7a9fcb13dad99
matthew-estes <matthew.estes19@gmail.com>
2024-07-14 11:05:05 -0400

    define paymentform functional componnet and initialize states

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -1,2 +1,14 @@
 import React, { useState } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const PaymentForm = () => {
+    const [success, setSuccess] = useState(false);
+    const stripe = useStripe();
+    const elements = useElements();
+
+
+
+
+
+
+}
