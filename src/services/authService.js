92b3d801df96b7dd7a3ad44845a1b8825a270793
matthew-estes <matthew.estes19@gmail.com>
2024-07-15 09:48:35 -0400

    adjusted authservice to allow for signup

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,22 +3,18 @@ const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 const signup = async (formData) => {
   try {
     const res = await fetch(`${BACKEND_URL}/users/signup`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     });
     const json = await res.json();
-    if (json.err) {
-      throw new Error(json.err);
+    if (json.error) {
+      throw new Error(json.error);
     }
-    if (json.token) {
-      localStorage.setItem('token', json.token);
-      const user = JSON.parse(atob(json.token.split('.')[1]));
-      return user;
-    }
+    localStorage.setItem("token", json.token);
+    return json;
   } catch (err) {
-    console.log(err);
-    throw err;
+    throw new Error(err);
   }
 };
 
