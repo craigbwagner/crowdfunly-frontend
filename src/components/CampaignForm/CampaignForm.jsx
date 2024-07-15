9c0b14da172cad336e14aefdec65ed167bdc3467
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-15 17:49:44 -0400

    remove commented out console.log

--- src/components/CampaignForm/CampaignForm.jsx
+++ src/components/CampaignForm/CampaignForm.jsx
@@ -17,7 +17,6 @@ const CampaignForm = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // console.log("formData", formData);
     if (campaignId) {
       props.handleUpdateCampaign(campaignId, formData);
     } else {
