974593560d0cb5ca756bc44823b800e986062cf8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-16 13:36:00 -0400

    remove old filter and display of campaigns list

--- src/components/CampaignsList/CampaignsList.jsx
+++ src/components/CampaignsList/CampaignsList.jsx
@@ -55,35 +55,9 @@ function CampaignsList({ campaigns }) {
     setRows();
   }, [campaigns]);
 
-  const handleTypeChange = (event) => {
-    setSelectedType(event.target.value);
-  };
-
-  const filteredCampaigns = selectedType
-    ? campaigns.filter((campaign) => campaign.campaignType === selectedType)
-    : campaigns;
-
   return (
     <div className="campaigns-container">
       <h1>Available Campaigns</h1>
-      <label htmlFor="campaignFilterType">Filter By Campaign Type: </label>
-      <select
-        name="campaignType"
-        id="campaignFilterType"
-        value={selectedType}
-        onChange={handleTypeChange}
-      >
-        <option value="">All</option>
-        <option value="Charity">Charity</option>
-        <option value="Education">Education</option>
-        <option value="Creative">Creative</option>
-        <option value="Sports">Sports</option>
-        <option value="Entertainment">Entertainment</option>
-        <option value="Business">Business</option>
-        <option value="Events">Events</option>
-        <option value="Environment">Environment</option>
-      </select>
-
       <div className="ag-theme-quartz" style={{ height: 500 }}>
         <AgGridReact
           rowData={rowData}
@@ -93,35 +67,6 @@ function CampaignsList({ campaigns }) {
           paginationPageSizeSelector={[20, 10, 5]}
         />
       </div>
-
-      <ul>
-        {campaigns.length === 0 ? (
-          <p>No campaigns found.</p>
-        ) : filteredCampaigns.length > 0 ? (
-          filteredCampaigns.map((campaign) => (
-            <li key={campaign._id}>
-              <h2>
-                <Link to={`/campaigns/${campaign._id}`}>{campaign.title}</Link>
-              </h2>
-              <p>{campaign.description}</p>
-              <p>Goal: ${campaign.goalAmount}</p>
-              <p>Raised: ${campaign.amountRaised}</p>
-              <p>
-                End Date:{" "}
-                {new Date(campaign.endDate).toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric",
-                  timeZone: "UTC",
-                })}
-              </p>
-              <p>Type: {campaign.campaignType}</p>
-            </li>
-          ))
-        ) : (
-          <p>No {selectedType} campaigns found.</p>
-        )}
-      </ul>
     </div>
   );
 }
