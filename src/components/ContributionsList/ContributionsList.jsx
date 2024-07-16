a9c966ad85e8734fb90a8c5c4e43fd1bac09e741
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-16 16:46:00 -0400

    adjust conditional rendering to account for not having userContributions passed down from ShowPage component

--- src/components/ContributionsList/ContributionsList.jsx
+++ src/components/ContributionsList/ContributionsList.jsx
@@ -1,37 +1,57 @@
 import { Link } from "react-router-dom";
 
-const ContributionsList = ({ contributions, parentComponent }) => {
+const ContributionsList = ({
+  contributions,
+  parentComponent,
+  userContributions,
+}) => {
+  let userContributionsJSX = [];
   console.log(contributions);
+  if (userContributions) {
+    userContributionsJSX = userContributions.map((contribution) => (
+      <li key={contribution._id}>
+        <h3>
+          Campaign:&nbsp;
+          <Link to={`/campaigns/${contribution.campaignId._id}`}>
+            {contribution.campaignId.title}
+          </Link>
+        </h3>
+        <p>Amount: ${contribution.amount}</p>
+        <p>
+          Date:{" "}
+          {new Date(contribution.createdAt).toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+            timeZone: "UTC",
+          })}
+        </p>
+      </li>
+    ));
+  }
+  const contributionsJSX = contributions.map((contribution) => (
+    <li key={contribution._id}>
+      <h3>Contributor: {contribution.contributedBy.username}</h3>
+      <p>Amount: ${contribution.amount}</p>
+      <p>
+        Date:{" "}
+        {new Date(contribution.createdAt).toLocaleDateString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          timeZone: "UTC",
+        })}
+      </p>
+    </li>
+  ));
   return (
     <ul>
       {contributions.length === 0 ? (
         <p>No contributions found.</p>
+      ) : parentComponent === "profilePage" ? (
+        userContributionsJSX
       ) : (
-        contributions.map((contribution) => (
-          <li key={contribution._id}>
-            {parentComponent === "profilePage" ? (
-              <h3>
-                Campaign:&nbsp;
-                <Link to={`/campaigns/${contribution.campaignId._id}`}>
-                  {contribution.campaignId.title}
-                </Link>
-              </h3>
-            ) : (
-              <h3>Contributor: {contribution.contributedBy.username}</h3>
-            )}
-
-            <p>Amount: ${contribution.amount}</p>
-            <p>
-              Date:{" "}
-              {new Date(contribution.createdAt).toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric",
-                year: "numeric",
-                timeZone: "UTC",
-              })}
-            </p>
-          </li>
-        ))
+        contributionsJSX
       )}
     </ul>
   );
