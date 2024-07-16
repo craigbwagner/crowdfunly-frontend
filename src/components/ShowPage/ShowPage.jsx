ffca9d89029291fac15fb6cc3a2187027296be96
jcao91 <jcao91@yahoo.com>
2024-07-16 15:19:32 -0500

    create and import showpage.css

--- src/components/ShowPage/ShowPage.jsx
+++ src/components/ShowPage/ShowPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import * as campaignService from "../../services/campaignService";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./ShowPage.css";
 
 
 function ShowPage({ user, handleDeleteCampaign }) {
