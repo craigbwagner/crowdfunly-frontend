56edf095290a732bed3a9c0a20f13b86b800ab67
matthew-estes <matthew.estes19@gmail.com>
2024-07-16 13:41:08 -0400

    update contributionsList and import to profile page. need assistance populating the specific campaign name.

--- src/components/ContributionsList/ContributionsList.jsx
+++ src/components/ContributionsList/ContributionsList.jsx
@@ -1,38 +1,35 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
 
-const ContributionsList = ({ user, contributions }) => {
-  const navigate = useNavigate();
-  const userContributions = contributions.filter((contribution) => contribution.contributedBy._id === user._id);
+import { Link } from 'react-router-dom';
 
-  const handleViewCampaign = (campaignId) => {
-    navigate(`/campaigns/${campaignId}`);
-  };
-
+const ContributionsList = ({ contributions, campaigns }) => {
   return (
-    <div>
-      <h1>{user.username}'s Contributions</h1>
-
-      <h2>Contributions</h2>
-      {userContributions.length > 0 ? (
-        <ul>
-          {userContributions.map((contribution) => (
-            <li key={contribution._id}>
-              <Link to={`/campaigns/${contribution.campaignId._id}`}>
-                {contribution.campaignId.title}
-              </Link>
-              <p>Amount: ${contribution.amount}</p>
-              <p>Date: {new Date(contribution.createdAt).toLocaleDateString()}</p>
-              <button onClick={() => handleViewCampaign(contribution.campaignId._id)}>
-                View Campaign
-              </button>
-            </li>
-          ))}
-        </ul>
+    <ul>
+      {contributions.length === 0 ? (
+        <p>No contributions found.</p>
       ) : (
-        <p>No contributions made yet.</p>
+        contributions.map((contribution) => (
+          <li key={contribution._id}>
+            <h2>
+              <Link to={`/campaigns/${contribution.campaignId}`}>
+                {campaigns.find(campaign => campaign._id === contribution.campaignId)?.title}
+              </Link>
+            </h2>
+            <p>Contributor: {contribution.contributedBy.username}</p>
+            <p>Amount: ${contribution.amount}</p>
+            <p>
+              Date:{" "}
+              {new Date(contribution.createdAt).toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+                timeZone: "UTC",
+              })}
+            </p>
+            <p>Comment: {contribution.comment}</p>
+          </li>
+        ))
       )}
-    </div>
+    </ul>
   );
 };
 
