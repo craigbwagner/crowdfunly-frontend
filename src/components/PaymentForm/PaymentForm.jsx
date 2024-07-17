25c195bb49677aeecd0412facf989b78fd22d284
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-17 16:32:04 -0400

    remove https prepended to stripe fetch url

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -59,7 +59,7 @@ const PaymentForm = ({ user }) => {
 
   const createPaymentIntent = async (amount) => {
     const response = await fetch(
-      `https://${import.meta.env.VITE_EXPRESS_BACKEND_URL}/stripe/create-payment-intent`,
+      `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/stripe/create-payment-intent`,
       {
         method: "POST",
         headers: {
@@ -74,7 +74,7 @@ const PaymentForm = ({ user }) => {
 
   const addContribution = async () => {
     const response = await fetch(
-      `https://${import.meta.env.VITE_EXPRESS_BACKEND_URL}/contributions/${campaignId}`,
+      `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/contributions/${campaignId}`,
       {
         method: "POST",
         headers: {
